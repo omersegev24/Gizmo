abb4a67d82aa337872bea94038034d11608ec7f3
orpartz <orptzz@gmail.com>
2020-03-20 22:55:42 +0200

    active editor tabs update

--- src/cmps/editor-sidebar.vue
+++ src/cmps/editor-sidebar.vue
@@ -2,8 +2,8 @@
   <section class="editor-sidebar">
     
     <div class="top-sidebar flex">
-      <div class="add" @click="editMode = false">Elements</div>
-      <div class="edit" @click="editMode = true">Edit</div>
+      <div class="add" :class="{'active-tab': isElsTabOn}" @click="isElsTabOn = !isElsTabOn">Elements</div>
+      <div class="edit" :class="{'active-tab': isEditTabOn}" @click="isEditTabOn = !isEditTabOn">Edit</div>
     </div>
 
     <edit-panel v-if="currCmp && editMode" :currCmp="currCmp"></edit-panel>
@@ -66,7 +66,9 @@ export default {
         }
       ],
       editMode: false,
-      currCmp: null
+      currCmp: null,
+      isElsTabOn: false,
+      isEditTabOn: false,
     };
   },
   created() {
