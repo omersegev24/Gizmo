758c8cf1b1d6c1aea349f9a4677b09f6a15f54e7
omer segev <omer@macbook-pro.mynet>
2020-03-23 17:24:52 +0200

    implement text editor

--- src/cmps/wap-cmps/app-header.vue
+++ src/cmps/wap-cmps/app-header.vue
@@ -11,33 +11,16 @@
       :style="child.style"
       :contenteditable="true"
       @blur="editTxt($event,child)"
+      @click.stop="openEdit(child)"
     >{{child.txt}}</component>
 
-    <!-- <h2
-      class="title"
-      :class="{editable: editMode}"
-      :contenteditable="editMode"
-      @blur="editTitle"
-    >{{currCmp.info.title}}</h2>
-    <h3
-      class="sub-title"
-      :class="{editable: editMode}"
-      :contenteditable="editMode"
-      @blur="editSubTitle"
-    >{{currCmp.info.subTitle}}</h3>
-
-    <button
-      :class="{editable: editMode}"
-      :contenteditable="editMode"
-      @blur="editButton"
-    >{{currCmp.info.callToAction}}</button>-->
   </header>
 </template>
 
 <script>
 import { eventBus } from "../../services/eventBus.service.js";
-import elTitle from '../wap-elements-cmp/title.cmp.vue'
-import elButton from '../wap-elements-cmp/button.cmp.vue'
+import elTitle from "../wap-elements-cmp/title.cmp.vue";
+import elButton from "../wap-elements-cmp/button.cmp.vue";
 export default {
   props: {
     cmp: Object
@@ -60,17 +43,8 @@ export default {
       cmpCopy.txt = ev.target.innerText;
       eventBus.$emit("updateCmp", cmpCopy);
     },
-    editSubTitle(ev) {
-      this.currCmp.info.subTitle = ev.target.innerText;
-      this.update();
-    },
-    editButton(ev) {
-      this.currCmp.info.callToAction = ev.target.innerText;
-      this.update();
-    },
-    update() {
-      var cmpCopy = JSON.parse(JSON.stringify(this.currCmp));
-      eventBus.$emit("updateCmp", cmpCopy);
+    openEdit(cmp){
+      eventBus.$emit('edit', cmp)
     }
   },
   components: {
