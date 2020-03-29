891150b6d737042185035ce0d794ff2d981b7a68
orpartz <orptzz@gmail.com>
2020-03-29 17:23:21 +0300

    start of homepage style

--- src/cmps/wap-list.vue
+++ src/cmps/wap-list.vue
@@ -2,14 +2,14 @@
   <section class="wap-list card-grid">
     <div v-for="wap in waps" :key="wap._id">
       <wap-preview :wap="wap"></wap-preview>
-      <template-action :wapId="wap._id"></template-action>
+      <template-action class="template-actions" :wapId="wap._id"></template-action>
     </div>
   </section>
 </template>
 
 <script>
 import wapPreview from "./wap-preview.vue";
-import templateAction from './template-action.vue'
+import templateAction from './template-action.vue';
 export default {
   props: {
     waps: Array
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="home-page">
-    <header>
-      <h1 class="logo"><router-link to="/">Gizmo</router-link></h1>
-      <!-- <img src="@/assets/img/homepage-header.png" alt="Header Image"> -->
+    <header class="flex space-evenly">
+      <div>
+        <div class="hero-content flex flex-column justify-center">
+          <h1 class="title">Create a Website You’re Proud Of</h1>
+          <p class="sub-title">
+            Discover the platform that gives you the freedom to create, design,
+            manage and develop your web presence exactly the way you want.
+          </p>
+          <router-link to="/template">Get Started</router-link>
+          <!-- <span>
+            <i class="fas fa-arrow-down"></i>
+          </span> -->
+        </div>
+      </div>
+      <img src="@/assets/img/homepage-header.png" alt="Header Image" />
     </header>
+    <section class="explore">
+      <h2 class="templates-title">Get Inspired by These Website Examples Designed on Gizmo</h2>
+      <hr>
+      <wap-list :waps="waps"></wap-list>
+    </section>
     <!-- <h1 class="title">Create a Website You’re Proud Of</h1>
     <div>
       <p class="sub-title">
@@ -12,10 +29,6 @@
       </p>
       <router-link to="/template">Get Started</router-link>
     </div>
-    <section class="explore-page main-layout">
-      <h2 class="title">Get Inspired by These Website Examples Designed on Gizmo</h2>
-      <wap-list :waps="waps"></wap-list>
-    </section>
     <button class="show-more-btn" @click="showMore">Show More...</button>-->
   </div>
 </template>
@@ -25,7 +38,6 @@ import wapList from "../cmps/wap-list.vue";
 export default {
   created() {
     this.$store.dispatch({ type: "loadWaps" });
-    
   },
   computed: {
     waps() {
@@ -40,8 +52,8 @@ export default {
     },
     // showMore() {
     //   this.$router.push("/template");
-    showMore(){
-      this.$router.push('/template')
+    showMore() {
+      this.$router.push("/template");
     }
   },
   components: {
