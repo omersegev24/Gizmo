cfd0cdb7c941169f3017d5b6a7bdc516d8c18453
orpartz <orptzz@gmail.com>
2020-03-31 13:30:57 +0300

    homepage

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,34 @@
 <template>
-  <div class="home-page">
-    <header class="flex space-evenly">
+  <div class="home-page flex flex-column justify-center">
+    <header class="hero flex flex-column justify-center align-center">
+      <section class="hero-title">Build your new responsive website in minutes</section>
+      <p>Our innoviate system and beautiful templates allow you to build any type of website.</p>
+      <button>Get Started</button>
+    </header>
+    <section class="infos flex flex-wrap">
+      <section class="info-1 flex flex-column space-between align-center">
+        <div>
+          <h1>Drag and Drop</h1>
+          <p>Use our powerful drag and drop feature for the best website building experience.</p>
+        </div>
+        <img src="@/assets/img/info-1.png" alt="Drag and Drop Image" />
+      </section>
+      <section class="info-2 info-1 flex flex-column space-between align-center">
+        <div>
+          <h1>Responsive</h1>
+          <p>Build fast and responsive website and make it look beautiful even on mobile and tablet.</p>
+        </div>
+        <img src="@/assets/img/info-2.png" alt="Responsive Image" />
+      </section>
+      <section class="info-3 info-1 flex flex-column space-between align-center">
+        <div>
+          <h1>No Code</h1>
+          <p>Find out how easy it is to build a powerful responsive website, with our system.</p>
+        </div>
+        <img src="@/assets/img/info-3.png" alt="No Code Image" />
+      </section>
+    </section>
+    <!-- <header class="flex space-evenly">
       <div>
         <div class="hero-content flex flex-column justify-center">
           <h1 class="title">Create a Website You’re Proud Of</h1>
@@ -9,9 +37,6 @@
             manage and develop your web presence exactly the way you want.
           </p>
           <router-link to="/template">Get Started</router-link>
-          <!-- <span>
-            <i class="fas fa-arrow-down"></i>
-          </span> -->
         </div>
       </div>
       <img src="@/assets/img/homepage-header.png" alt="Header Image" />
@@ -20,16 +45,7 @@
       <h2 class="templates-title">Get Inspired by These Website Examples Designed on Gizmo</h2>
       <hr>
       <wap-list class="templates-list" :waps="waps"></wap-list>
-    </section>
-    <!-- <h1 class="title">Create a Website You’re Proud Of</h1>
-    <div>
-      <p class="sub-title">
-        Discover the platform that gives you the freedom to create, design,
-        manage and develop your web presence exactly the way you want.
-      </p>
-      <router-link to="/template">Get Started</router-link>
-    </div>
-    <button class="show-more-btn" @click="showMore">Show More...</button>-->
+    </section>-->
   </div>
 </template>
 <script>
