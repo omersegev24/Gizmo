d4c794b86f65390511c851135e13479b65af9eac
orpartz <orptzz@gmail.com>
2020-03-31 13:54:48 +0300

    app-nav mobile

--- src/views/home.vue
+++ src/views/home.vue
@@ -28,24 +28,6 @@
         <img src="@/assets/img/info-3.png" alt="No Code Image" />
       </section>
     </section>
-    <!-- <header class="flex space-evenly">
-      <div>
-        <div class="hero-content flex flex-column justify-center">
-          <h1 class="title">Create a Website You’re Proud Of</h1>
-          <p class="sub-title">
-            Discover the platform that gives you the freedom to create, design,
-            manage and develop your web presence exactly the way you want.
-          </p>
-          <router-link to="/template">Get Started</router-link>
-        </div>
-      </div>
-      <img src="@/assets/img/homepage-header.png" alt="Header Image" />
-    </header>
-    <section class="explore">
-      <h2 class="templates-title">Get Inspired by These Website Examples Designed on Gizmo</h2>
-      <hr>
-      <wap-list class="templates-list" :waps="waps"></wap-list>
-    </section>-->
   </div>
 </template>
 <script>
@@ -64,10 +46,7 @@ export default {
   methods: {
     async openEditor(wapId) {
       this.$router.push(`/editor/${wapId}`).catch(err => {});
-      // this.$router.push(`/editor/${wapId}`);
     },
-    // showMore() {
-    //   this.$router.push("/template");
     showMore() {
       this.$router.push("/template");
     }
